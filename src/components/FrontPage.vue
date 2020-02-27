1eb209e9e69b55b996c7a8277853f62d263a3669
Kristoffer Hellenäs <kristoffer_hellenas@hotmail.se>
2020-02-27 15:15:11 +0100

    Updated FrontPage

--- src/components/FrontPage.vue
+++ src/components/FrontPage.vue
@@ -1,13 +1,14 @@
 <template>
   <div id="frontpage">
+    <h1>Eventsidan</h1>
     <div class="container-sm">
-      <h1>Eventsidan</h1>
-      <b-form-input id="citylist" list="my-list-id"></b-form-input>
-
-      <datalist id="my-list-id">
-        <option>Manual Option</option>
-        <option v-for="city in cities" :key="city.route" @click="enterCityPage">{{ city.name }}</option>
-      </datalist>
+      <div class="row align-items-center" id="buttonLinks" v-for="city in cities" :key="city.route">
+        <div class="col align-self-center">
+          <router-link to="/cities">
+            <button type="button" class="btn btn-success btn-lg">{{ city.name }}</button>
+          </router-link>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -16,33 +17,40 @@
 export default {
   data() {
     return {
+      chosenCity: null,
       cities: [
-        { name: "Göteborg", route: "göteborg" },
+        { name: "Göteborg", route: "cities" },
         { name: "Stockholm", route: "stockholm" },
         { name: "Malmö", route: "malmö" }
       ]
     };
-  },
-  methods: {
-    enterCityPage() {}
   }
 };
 </script>
 
 <style scoped>
-#citylist {
-  width: 50%;
-  height: 60px;
-  font-size: 1.5rem;
-  display: inline-block;
-  text-align: center;
-  margin-top: 30%;
-}
-#citylist option {
-  font-size: 3rem;
-}
 #frontpage {
-  background-image: url("../assets/img/startpage.jpg");
   height: 100vh;
+  background-image: linear-gradient(
+      to bottom,
+      rgba(240, 240, 240, 0.8),
+      rgba(0, 0, 0, 0)
+    ),
+    url("../assets/img/fireworks.jpg");
+  background-size: cover;
+  width: 100%;
+  background-position-x: 50%;
+  background-position-y: 100%;
+}
+h1 {
+  font-size: 5rem;
+  padding-top: 15%;
+}
+button {
+  font-size: 2rem;
+  width: 340px;
+}
+#buttonLinks {
+  margin-top: 5%;
 }
 </style>
