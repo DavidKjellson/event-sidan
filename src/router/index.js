c5de0fe3b33d1afa31ae7ea86d2d1b36f2e896cb
puttzon <puttzon@gmail.com>
2020-02-27 13:00:28 +0100

    Ändrat i router

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,30 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import FrontPage from '../components/FrontPage.vue'
-import HelloWorld from '../components/HelloWorld.vue'
+// import HelloWorld from '../components/HelloWorld.vue'
+
+import Cities from '../components/Cities'
 
 Vue.use(VueRouter)
 
 const routes = [{
-    path: '/',
-    name: 'FrontPage',
-    component: FrontPage,
-    meta: {
-      title: 'Eventsidan'
-    },
+  path: '/',
+  name: 'FrontPage',
+  component: FrontPage,
+  meta: {
+    title: 'Eventsidan'
   },
-  {
-    path: '/cities',
-    name: 'HelloWorld',
-    component: HelloWorld
-  }
+},
+// {
+//   path: '/cities',
+//   name: 'HelloWorld',
+//   component: HelloWorld
+// },
+{
+  path: '/cities',
+  name: 'Cities',
+  component: Cities
+}
 ]
 
 const router = new VueRouter({
