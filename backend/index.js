831e277767fd5e05f0171e7125023a43e08e38aa
David Kjellson <david.kjellson@gmail.com>
2020-03-11 09:57:36 +0100

    Till kollegorna

--- backend/index.js
+++ backend/index.js
@@ -8,4 +8,39 @@ app.use(cors())
 
 let database
 
+sqlite.open('events.sqlite')
+  .then(database_ => {
+    database = database_
+  })
+
+app.get('/', (request, response) => {
+  database.all('SELECT * FROM events')
+    .then(events => {
+      database.all('SELECT * FROM CITIES')
+        .then(cities => {
+          response.send(cities)
+        })
+      const o = []
+
+      for (i = 0; i < cities.length; i++) {
+
+        o.push(cities)
+
+        for (i = 0; i < events.length; i++) {
+
+          if (events.location === cities.location) {
+            o.push(events)
+
+          }
+        }
+
+      }
+
+      response.send(o)
+      response.send(events)
+      console.log(o)
+
+    })
+})
+
 app.listen(3000)
