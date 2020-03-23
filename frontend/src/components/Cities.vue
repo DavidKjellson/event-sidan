92cf6ebfd46a8694cdc9627a8746add8a3a615fd
David Kjellson <david.kjellson@gmail.com>
2020-03-23 15:37:06 +0100

    Header är en egen komponent.

--- frontend/src/components/Cities.vue
+++ frontend/src/components/Cities.vue
@@ -1,45 +1,7 @@
 <template>
   <div>
     <header>
-      <div class="collapse bg-transparent" id="navbarHeader">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-6 py-4"></div>
-            <div class="col-md-6 py-4">
-              <!-- <h4 class="text-dark">Contact</h4>
-              <ul class="list-unstyled">
-                <li>
-                  <a href="#" class="text-dark">Follow on Twitter</a>
-                </li>
-                <li>
-                  <a href="#" class="text-dark">Like on Facebook</a>
-                </li>
-                <li>
-                  <a href="#" class="text-dark">Email me</a>
-                </li>
-              </ul>-->
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="navbar navbar-light bg-transparent">
-        <div class="container d-flex justify-content-between">
-          <router-link to="/" class="navbar-brand d-flex align-items-center">
-            <h1 class="text-dark" id="name">Activityfy</h1>
-          </router-link>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarHeader"
-            aria-controls="navbarHeader"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-        </div>
-      </div>
+      <Header />
     </header>
 
     <!-- HERO WITH IMG -->
@@ -239,8 +201,13 @@ import secret from "../secret";
   }
 })(document, "script", "weatherwidget-io-js");
 
+import Header from "./Header.vue";
+
 export default {
   name: "Cities",
+  components: {
+    Header
+  },
   data() {
     return {
       events: [],
@@ -438,10 +405,6 @@ select:invalid {
   color: gray;
 }
 
-#name {
-  font-family: "Abel", sans-serif;
-  font-size: 30px;
-}
 @media screen and (max-width: 992px) {
   iframe {
     width: 75vw;
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <div class="collapse bg-transparent" id="navbarHeader">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 py-4"></div>
+          <div class="col-md-6 py-4">
+            <!-- <h4 class="text-dark">Contact</h4>
+              <ul class="list-unstyled">
+                <li>
+                  <a href="#" class="text-dark">Follow on Twitter</a>
+                </li>
+                <li>
+                  <a href="#" class="text-dark">Like on Facebook</a>
+                </li>
+                <li>
+                  <a href="#" class="text-dark">Email me</a>
+                </li>
+            </ul>-->
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="navbar navbar-light bg-transparent">
+      <div class="container d-flex justify-content-between">
+        <router-link to="/" class="navbar-brand d-flex align-items-center">
+          <h1 class="text-dark" id="name">Activityfy</h1>
+        </router-link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarHeader"
+          aria-controls="navbarHeader"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style scoped>
+#name {
+  font-family: "Abel", sans-serif;
+  font-size: 30px;
+}
+</style>
