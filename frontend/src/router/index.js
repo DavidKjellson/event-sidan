215b8222426f2ba546b8a33c85c6b4debc8fb130
puttzon <puttzon@gmail.com>
2020-03-25 14:17:45 +0100

    ABout sida

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,25 +1,31 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import FrontPage from '../components/FrontPage.vue'
+import About from '../components/About.vue'
 
 import Cities from '../components/Cities'
 
 Vue.use(VueRouter)
 
 const routes = [{
-    path: '/',
-    name: 'FrontPage',
-    component: FrontPage,
-    meta: {
-      title: 'Activityfy'
-    },
+  path: '/',
+  name: 'FrontPage',
+  component: FrontPage,
+  meta: {
+    title: 'Activityfy'
   },
-  {
-    path: '/cities/:city',
-    name: 'Cities',
-    component: Cities,
+},
+{
+  path: '/cities/:city',
+  name: 'Cities',
+  component: Cities,
 
-  }
+},
+{
+  path: '/about',
+  name: 'About',
+  component: About
+}
 ]
 
 const router = new VueRouter({
